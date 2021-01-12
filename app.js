4b606abf72b1b5135543fe3a300d8f91edfdea22
jcramem <72235271+jcramem@users.noreply.github.com>
2021-01-12 12:31:50 -0600

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ function updateFilters() {
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
  
-    if elementValue {
+    if (elementValue) {
       filters[filterId] = elementValue ;  
     } 
     else {
@@ -68,13 +68,13 @@ function updateFilters() {
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
 
-    Object.keys(obj).forEach(([key, value]) => 
+    Object.entries(filters).forEach(([key, value]) => 
        filteredData = filteredData.filter(row => row[key] === value));   
     }
   
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
-  }
+  
   
   // 2. Attach an event to listen for changes to each filter
   d3.selectAll("input").on("change", updateFilters);
