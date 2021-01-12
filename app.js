ebfd66bfda43a26f03524ee6632fa94fdad10189
jcramem <72235271+jcramem@users.noreply.github.com>
2021-01-12 12:45:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -69,9 +69,9 @@ function updateFilters() {
     // matches the filter values
 
     Object.entries(filters).forEach(([key, value]) => {
-       filteredData = filteredData.filter(row => row[key] === value);   
-    }
-  
+       filteredData = filteredData.filter(row => row[key] === value);
+    });
+      
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
   
